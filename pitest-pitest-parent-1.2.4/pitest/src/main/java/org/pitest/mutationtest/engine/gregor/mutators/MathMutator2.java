ef74d3750d01e3ec86f97e2ab299b87ab88f2abf
matthewchen67 <matthewchen67@gmail.com>
2017-10-30 20:32:08 -0500

    Added more mutators to MathMutators

--- pitest-pitest-parent-1.2.4/pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/MathMutator2.java
+++ pitest-pitest-parent-1.2.4/pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/MathMutator2.java
@@ -26,14 +26,14 @@ import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
 import org.pitest.mutationtest.engine.gregor.MutationContext;
 import org.pitest.mutationtest.engine.gregor.ZeroOperandMutation;
 
-public enum MathMutator implements MethodMutatorFactory {
+public enum MathMutator2 implements MethodMutatorFactory {
 
   MATH_MUTATOR;
 
   @Override
   public MethodVisitor create(final MutationContext context,
       final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
-    return new MathMethodVisitor(this, methodInfo, context, methodVisitor);
+    return new MathMethodVisitor2(this, methodInfo, context, methodVisitor);
   }
 
   @Override
@@ -48,9 +48,9 @@ public enum MathMutator implements MethodMutatorFactory {
 
 }
 
-class MathMethodVisitor extends AbstractInsnMutator {
+class MathMethodVisitor2 extends AbstractInsnMutator {
 
-  MathMethodVisitor(final MethodMutatorFactory factory,
+  MathMethodVisitor2(final MethodMutatorFactory factory,
       final MethodInfo methodInfo, final MutationContext context,
       final MethodVisitor writer) {
     super(factory, methodInfo, context, writer);
@@ -59,20 +59,20 @@ class MathMethodVisitor extends AbstractInsnMutator {
   private static final Map<Integer, ZeroOperandMutation> MUTATIONS = new HashMap<Integer, ZeroOperandMutation>();
 
   static {
-    MUTATIONS.put(Opcodes.IADD, new InsnSubstitution(Opcodes.ISUB,
-        "Replaced integer addition with subtraction"));
-    MUTATIONS.put(Opcodes.ISUB, new InsnSubstitution(Opcodes.IADD,
-        "Replaced integer subtraction with addition"));
-    MUTATIONS.put(Opcodes.IMUL, new InsnSubstitution(Opcodes.IDIV,
-        "Replaced integer multiplication with division"));
-    MUTATIONS.put(Opcodes.IDIV, new InsnSubstitution(Opcodes.IMUL,
-        "Replaced integer division with multiplication"));
+    MUTATIONS.put(Opcodes.IADD, new InsnSubstitution(Opcodes.IMUL,
+        "Replaced integer addition with multiplication"));
+    MUTATIONS.put(Opcodes.ISUB, new InsnSubstitution(Opcodes.IDIV,
+        "Replaced integer subtraction with division"));
+    MUTATIONS.put(Opcodes.IMUL, new InsnSubstitution(Opcodes.IADD,
+        "Replaced integer multiplication with addition"));
+    MUTATIONS.put(Opcodes.IDIV, new InsnSubstitution(Opcodes.ISUB,
+        "Replaced integer division with subtraction"));
     MUTATIONS.put(Opcodes.IOR, new InsnSubstitution(Opcodes.IAND,
         "Replaced bitwise OR with AND"));
     MUTATIONS.put(Opcodes.IAND, new InsnSubstitution(Opcodes.IOR,
         "Replaced bitwise AND with OR"));
-    MUTATIONS.put(Opcodes.IREM, new InsnSubstitution(Opcodes.IMUL,
-        "Replaced integer modulus with multiplication"));
+    MUTATIONS.put(Opcodes.IREM, new InsnSubstitution(Opcodes.IDIV,
+        "Replaced integer modulus with division"));
     MUTATIONS.put(Opcodes.IXOR, new InsnSubstitution(Opcodes.IAND,
         "Replaced XOR with AND"));
     MUTATIONS.put(Opcodes.ISHL, new InsnSubstitution(Opcodes.ISHR,
@@ -83,20 +83,20 @@ class MathMethodVisitor extends AbstractInsnMutator {
         "Replaced Unsigned Shift Right with Shift Left"));
 
     // longs
-    MUTATIONS.put(Opcodes.LADD, new InsnSubstitution(Opcodes.LSUB,
-        "Replaced long addition with subtraction"));
-    MUTATIONS.put(Opcodes.LSUB, new InsnSubstitution(Opcodes.LADD,
-        "Replaced long subtraction with addition"));
-    MUTATIONS.put(Opcodes.LMUL, new InsnSubstitution(Opcodes.LDIV,
-        "Replaced long multiplication with division"));
-    MUTATIONS.put(Opcodes.LDIV, new InsnSubstitution(Opcodes.LMUL,
-        "Replaced long division with multiplication"));
+    MUTATIONS.put(Opcodes.LADD, new InsnSubstitution(Opcodes.LMUL,
+        "Replaced long addition with multiplication"));
+    MUTATIONS.put(Opcodes.LSUB, new InsnSubstitution(Opcodes.LDIV,
+        "Replaced long subtraction with division"));
+    MUTATIONS.put(Opcodes.LMUL, new InsnSubstitution(Opcodes.LADD,
+        "Replaced long multiplication with addition"));
+    MUTATIONS.put(Opcodes.LDIV, new InsnSubstitution(Opcodes.LSUB,
+        "Replaced long division with subtraction"));
     MUTATIONS.put(Opcodes.LOR, new InsnSubstitution(Opcodes.LAND,
         "Replaced bitwise OR with AND"));
     MUTATIONS.put(Opcodes.LAND, new InsnSubstitution(Opcodes.LOR,
         "Replaced bitwise AND with OR"));
-    MUTATIONS.put(Opcodes.LREM, new InsnSubstitution(Opcodes.LMUL,
-        "Replaced long modulus with multiplication"));
+    MUTATIONS.put(Opcodes.LREM, new InsnSubstitution(Opcodes.LDIV,
+        "Replaced long modulus with division"));
     MUTATIONS.put(Opcodes.LXOR, new InsnSubstitution(Opcodes.LAND,
         "Replaced XOR with AND"));
     MUTATIONS.put(Opcodes.LSHL, new InsnSubstitution(Opcodes.LSHR,
@@ -107,28 +107,28 @@ class MathMethodVisitor extends AbstractInsnMutator {
         "Replaced Unsigned Shift Right with Shift Left"));
 
     // floats
-    MUTATIONS.put(Opcodes.FADD, new InsnSubstitution(Opcodes.FSUB,
-        "Replaced float addition with subtraction"));
-    MUTATIONS.put(Opcodes.FSUB, new InsnSubstitution(Opcodes.FADD,
-        "Replaced float subtraction with addition"));
-    MUTATIONS.put(Opcodes.FMUL, new InsnSubstitution(Opcodes.FDIV,
-        "Replaced float multiplication with division"));
-    MUTATIONS.put(Opcodes.FDIV, new InsnSubstitution(Opcodes.FMUL,
-        "Replaced float division with multiplication"));
-    MUTATIONS.put(Opcodes.FREM, new InsnSubstitution(Opcodes.FMUL,
-        "Replaced float modulus with multiplication"));
+    MUTATIONS.put(Opcodes.FADD, new InsnSubstitution(Opcodes.FMUL,
+        "Replaced float addition with multiplication"));
+    MUTATIONS.put(Opcodes.FSUB, new InsnSubstitution(Opcodes.FDIV,
+        "Replaced float subtraction with division"));
+    MUTATIONS.put(Opcodes.FMUL, new InsnSubstitution(Opcodes.FSUB,
+        "Replaced float multiplication with subtraction"));
+    MUTATIONS.put(Opcodes.FDIV, new InsnSubstitution(Opcodes.FADD,
+        "Replaced float division with addition"));
+    MUTATIONS.put(Opcodes.FREM, new InsnSubstitution(Opcodes.FDIV,
+        "Replaced float modulus with division"));
 
     // doubles
-    MUTATIONS.put(Opcodes.DADD, new InsnSubstitution(Opcodes.DSUB,
-        "Replaced double addition with subtraction"));
-    MUTATIONS.put(Opcodes.DSUB, new InsnSubstitution(Opcodes.DADD,
-        "Replaced double subtraction with addition"));
-    MUTATIONS.put(Opcodes.DMUL, new InsnSubstitution(Opcodes.DDIV,
-        "Replaced double multiplication with division"));
-    MUTATIONS.put(Opcodes.DDIV, new InsnSubstitution(Opcodes.DMUL,
-        "Replaced double division with multiplication"));
-    MUTATIONS.put(Opcodes.DREM, new InsnSubstitution(Opcodes.DMUL,
-        "Replaced double modulus with multiplication"));
+    MUTATIONS.put(Opcodes.DADD, new InsnSubstitution(Opcodes.DMUL,
+        "Replaced double addition with multiplication"));
+    MUTATIONS.put(Opcodes.DSUB, new InsnSubstitution(Opcodes.DDIV,
+        "Replaced double subtraction with division"));
+    MUTATIONS.put(Opcodes.DMUL, new InsnSubstitution(Opcodes.DADD,
+        "Replaced double multiplication with addition"));
+    MUTATIONS.put(Opcodes.DDIV, new InsnSubstitution(Opcodes.DSUB,
+        "Replaced double division with subtraction"));
+    MUTATIONS.put(Opcodes.DREM, new InsnSubstitution(Opcodes.DDIV,
+        "Replaced double modulus with division"));
 
   }
 
